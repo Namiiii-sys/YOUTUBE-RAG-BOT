8784f8fd5ff8548531c36eca6f26c0246a426044
Namiiii-sys <Namitamehra207@gmail.com>
2025-07-21 11:42:05 +0530

    popup ui done

--- src/components/Chatinterface.tsx
+++ src/components/Chatinterface.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import { BsFillSendFill } from "react-icons/bs"
 
+
 type ChatMessage = {
   type: "user" | "bot";
   text: string;
@@ -12,6 +13,22 @@ const Chatinterface = () => {
   const [loading, setloading] = useState(false)
   const [question, setquestion] = useState("")
 
+  const getCurrentTabUrl = (): Promise<string> => {
+  return new Promise((resolve) => {
+
+    chrome.runtime.sendMessage({ type: "GET_CURRENT_TAB_URL" }, (response) => {
+        if (chrome.runtime.lastError) {
+      console.error("Error getting URL:", chrome.runtime.lastError);
+    } else {
+      console.log("Fetched URL from background:", response.url); // 👈 check this
+    }
+      resolve(response.url);
+     });
+     
+    });
+  };
+
+
   const handleAsk = async () => {
     if (!question.trim()) return;
 
@@ -19,8 +36,9 @@ const Chatinterface = () => {
     setchat(prev => [...prev, { type: "user", text: question }]);
 
     try {
+      const url = await getCurrentTabUrl();
       const res = await fetch(
-        `http://localhost:8000/query?video_url=${encodeURIComponent(window.location.href)}&question=${encodeURIComponent(question)}`,
+        `http://localhost:8000/query?video_url=${encodeURIComponent(url)}&question=${encodeURIComponent(question)}`,
         {
           method: "POST",
         }
@@ -38,14 +56,14 @@ const Chatinterface = () => {
   };
 
   return (
-    <div className="w-[450px] h-[500px] px-6 py-4 overflow-hidden bg-gray-100 rounded-lg shadow-md flex flex-col">
+    <div className="w-[400px] h-[450px] px-6 py-5 overflow-hidden bg-gray-100 rounded-xl shadow-md flex flex-col">
       <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">YouTube Assistant</h1>
       
       <div className="h-3/4 w-full border border-indigo-950 rounded-lg p-3 overflow-y-auto space-y-2">
         {chat.map((msg, i) => (
           <div
             key={i}
-            className={`p-2 rounded-2xl text-sm w-fit max-w-[75%] ${msg.type === "user" ? "bg-black ml-auto text-white" : "bg-gray-200 text-black"}`}
+            className={`p-2 break-words whitespace-pre-wrap rounded-2xl text-sm w-fit max-w-[75%] ${msg.type === "user" ? "bg-black ml-auto text-white" : "bg-gray-300 text-black"}`}
           >
             {msg.text}
           </div>
